536da43dfa5f045ab0f61ab087e0287eafdee63a
srhgeorgia <sgeorgia0297@gmail.com>
2024-11-27 16:41:52 -0300

    exercicios aula

---
+++ Java/exercicios_aula/Alfabeto.java
@@ -0,0 +1,12 @@
+package exercicios_aula;
+
+public class Alfabeto {
+  public static void main(String[] args) {
+    char letra = 'A';
+
+    while (letra <= 'Z') {
+      System.out.println(letra);
+      letra++;
+    }
+  }
+}
--- Java/exercicios_aula/Calculadora.java
+++ Java/exercicios_aula/Calculadora.java
@@ -1,3 +1,5 @@
+package exercicios_aula;
+
 import java.util.Scanner;
 
 public class Calculadora {
